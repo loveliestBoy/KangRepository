ac108a6975188e213ead40babcfe8f1c3c612f11
Kang <70930661+Kang27@users.noreply.github.com>
2020-11-08 18:33:38 +0800

    Add files via upload

---
+++ CellMachine.java
@@ -0,0 +1,82 @@
+//     细胞自动机
+// 某细胞所处九宫格内(8个邻居)
+// 新生：如果正好有3个邻居活着
+// 死亡：活着的邻居数量 <2 或 >3
+// 其他情况则该细胞保持原状
+
+// 体现 数据（Field）与 表现（View）分离
+// 体现责任驱动设计
+// CellMachine类负责业务逻辑，其他类只负责自己
+// 网格化画面设计
+
+package cellmachine;
+
+import javax.swing.JFrame;
+
+import cell.Cell;
+import field.Field;
+import field.View;
+
+public class CellMachine {
+
+	public static void main(String[] args) {
+		Field field = new Field(30,30);
+		for ( int row = 0; row<field.getHeight(); row++ ) {
+			for ( int col = 0; col<field.getWidth(); col++ ) {
+				field.place(row, col, new Cell());
+			}
+		}
+		for ( int row = 0; row<field.getHeight(); row++ ) {
+			for ( int col = 0; col<field.getWidth(); col++ ) {
+				Cell cell = field.get(row, col);
+				if ( Math.random() < 0.2 ) {
+					cell.reborn();
+				}
+			}
+		}
+		View view = new View(field);
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setTitle("Cells");
+		frame.add(view);
+		frame.pack();
+		frame.setVisible(true);
+		
+		for ( int i=0; i<1000; i++ ) {
+			for ( int row = 0; row<field.getHeight(); row++ ) {
+				for ( int col = 0; col<field.getWidth(); col++ ) {
+					Cell cell = field.get(row, col);
+					Cell[] neighbour = field.getNeighbour(row, col);
+					int numOfLive = 0;
+					for ( Cell c : neighbour ) {
+						if ( c.isAlive() ) {
+							numOfLive++;
+						}
+					}
+					System.out.print("["+row+"]["+col+"]:");
+					System.out.print(cell.isAlive()?"live":"dead");
+					System.out.print(":"+numOfLive+"-->");
+					if ( cell.isAlive() ) {
+						if ( numOfLive <2 || numOfLive >3 ) {
+							cell.die();
+							System.out.print("die");
+						}
+					} else if ( numOfLive == 3 ) {
+						cell.reborn();
+						System.out.print("reborn");
+					}
+					System.out.println();
+				}
+			}
+			System.out.println("UPDATE");
+			frame.repaint();
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
